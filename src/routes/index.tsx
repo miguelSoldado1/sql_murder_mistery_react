850897f37fc3263d312944f01c74689126bb09bf
MIGUEL SOLDADO <miguel-a-soldado@telecom.pt>
2024-09-21 22:51:50 +0100

    index page and starting routing setup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,67 +1,40 @@
-import { useEffect, useState } from "react";
-import { createFileRoute } from "@tanstack/react-router";
-import initSqlJs, { type Database } from "sql.js";
-import { SolutionInput } from "@/components/solutionInput";
-import { SqlQuery, SqlQueryDescription, SqlQueryHeader, SqlQueryTitle } from "@/components/sqlQuery";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export const Route = createFileRoute("/")({
   component: App,
 });
 
 function App() {
-  const [db, setDb] = useState<Database | null>(null);
-
-  useEffect(() => {
-    async function initDB() {
-      const SQL = await initSqlJs({ locateFile: (file) => `https://sql.js.org/dist/${file}` });
-
-      // Fetch the SQLite database file from the public directory
-      const response = await fetch("/murder_mystery.db");
-      const buffer = await response.arrayBuffer();
-
-      // Load the database from the file
-      const database = new SQL.Database(new Uint8Array(buffer));
-      setDb(database);
-    }
-    initDB();
-  }, []);
-
   return (
     <div className="mx-auto my-6 w-4/5 space-y-12">
       <div className="space-y-4">
-        <h1 className="text-2xl font-bold">SQL Murder Mystery I</h1>
+        <h1 className="text-3xl font-semibold">SQL Challenges</h1>
         <p>
-          A crime has taken place and the detective needs your help. The detective gave you the crime scene report, but you
-          somehow lost it. You vaguely remember that the <strong>crime was a murder</strong> that occurred sometime on
-          <strong> Jan.15, 2018</strong> and that <strong>it took place in SQL City</strong>. Start by retrieving the
-          corresponding crime scene report from the police department's database. Below is the schema diagram for this
-          database.
+          Inspired by the classic{" "}
+          <a href="https://mystery.knightlab.com/" className="text-blue-500 underline">
+            SQL Murder Mystery
+          </a>
+          , this website takes the concept further by offering a variety of additional challenges. Immerse yourself in the
+          world of SQL and sharpen your problem-solving skills with some brand new engaging SQL challenges.
         </p>
-        <img src="/schema.png" className="mx-auto w-3/4" draggable={false} />
       </div>
-      <SqlQuery db={db} defaultValue="SELECT name FROM sqlite_master WHERE type = 'table'">
-        <SqlQueryHeader>
-          <SqlQueryTitle>Run this query to find the names of the tables in this database</SqlQueryTitle>
-          <SqlQueryDescription>
-            This command is specific to SQLite. For other databases, you'll have to learn their specific syntax.
-          </SqlQueryDescription>
-        </SqlQueryHeader>
-      </SqlQuery>
-      <SqlQuery db={db} defaultValue="SELECT sql FROM sqlite_master where name = 'crime_scene_report'">
-        <SqlQueryHeader>
-          <SqlQueryTitle>Find the structure of the `crime_scene_report` table</SqlQueryTitle>
-          <SqlQueryDescription>
-            This command is specific to SQLite. For other databases, you'll have to learn their specific syntax.
-          </SqlQueryDescription>
-        </SqlQueryHeader>
-      </SqlQuery>
-      <SqlQuery db={db}>
-        <SqlQueryHeader>
-          <SqlQueryTitle>Find who commited the murder</SqlQueryTitle>
-          <SqlQueryDescription>When you think you know the answer, go to the next section.</SqlQueryDescription>
-        </SqlQueryHeader>
-      </SqlQuery>
-      <SolutionInput />
+      <div className="inline-block min-w-full align-middle shadow ring-1 ring-accent">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="font-semibold">challenge</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="max-w-lg">
+                <Link to="/murder_mystery_i">SQL Murder Mistery I</Link>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
---
+++ src/routes/murder_mystery_i.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { createFileRoute } from "@tanstack/react-router";
+import initSqlJs, { type Database } from "sql.js";
+import { SolutionInput } from "@/components/solutionInput";
+import { SqlQuery, SqlQueryDescription, SqlQueryHeader, SqlQueryTitle } from "@/components/sqlQuery";
+
+export const Route = createFileRoute("/murder_mystery_i")({
+  component: App,
+});
+
+function App() {
+  const [db, setDb] = useState<Database | null>(null);
+
+  useEffect(() => {
+    async function initDB() {
+      const SQL = await initSqlJs({ locateFile: (file) => `https://sql.js.org/dist/${file}` });
+
+      // Fetch the SQLite database file from the public directory
+      const response = await fetch("/murder_mystery.db");
+      const buffer = await response.arrayBuffer();
+
+      // Load the database from the file
+      const database = new SQL.Database(new Uint8Array(buffer));
+      setDb(database);
+    }
+    initDB();
+  }, []);
+
+  return (
+    <div className="mx-auto my-6 w-4/5 space-y-12">
+      <div className="space-y-4">
+        <h1 className="text-2xl font-bold">SQL Murder Mystery I</h1>
+        <p>
+          A crime has taken place and the detective needs your help. The detective gave you the crime scene report, but you
+          somehow lost it. You vaguely remember that the <strong>crime was a murder</strong> that occurred sometime on
+          <strong> Jan.15, 2018</strong> and that <strong>it took place in SQL City</strong>. Start by retrieving the
+          corresponding crime scene report from the police department's database. Below is the schema diagram for this
+          database.
+        </p>
+        <img src="/schema.png" className="mx-auto w-3/4" draggable={false} />
+      </div>
+      <SqlQuery db={db} defaultValue="SELECT name FROM sqlite_master WHERE type = 'table'">
+        <SqlQueryHeader>
+          <SqlQueryTitle>Run this query to find the names of the tables in this database</SqlQueryTitle>
+          <SqlQueryDescription>
+            This command is specific to SQLite. For other databases, you'll have to learn their specific syntax.
+          </SqlQueryDescription>
+        </SqlQueryHeader>
+      </SqlQuery>
+      <SqlQuery db={db} defaultValue="SELECT sql FROM sqlite_master WHERE name = 'crime_scene_report'">
+        <SqlQueryHeader>
+          <SqlQueryTitle>Find the structure of the `crime_scene_report` table</SqlQueryTitle>
+          <SqlQueryDescription>
+            This command is specific to SQLite. For other databases, you'll have to learn their specific syntax.
+          </SqlQueryDescription>
+        </SqlQueryHeader>
+      </SqlQuery>
+      <SqlQuery db={db}>
+        <SqlQueryHeader>
+          <SqlQueryTitle>Find who commited the murder</SqlQueryTitle>
+          <SqlQueryDescription>When you think you know the answer, go to the next section.</SqlQueryDescription>
+        </SqlQueryHeader>
+      </SqlQuery>
+      <SolutionInput />
+    </div>
+  );
+}
+
