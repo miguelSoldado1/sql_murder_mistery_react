3bfe8a2962b509eeb61c0d318f0567f4659d02d1
MIGUEL SOLDADO <miguel-a-soldado@telecom.pt>
2024-09-21 16:24:08 +0100

    improved wrapper div in table

--- src/components/resultsTable.tsx
+++ src/components/resultsTable.tsx
@@ -9,31 +9,29 @@ export function ResultsTable({ results }: ResultsTableProps) {
   if (results.length <= 0) return null;
 
   return (
-    <div className="inline-block min-w-full align-middle">
-      <div className="shadow ring-1 ring-accent ring-opacity-5">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              {results[0].columns.map((column, i) => (
-                <TableHead key={i} className="font-semibold">
-                  {column}
-                </TableHead>
+    <div className="inline-block min-w-full align-middle shadow ring-1 ring-accent ring-opacity-5">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            {results[0].columns.map((column, i) => (
+              <TableHead key={i} className="font-semibold">
+                {column}
+              </TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {results[0].values.map((row, i) => (
+            <TableRow key={i} className={i % 2 === 0 ? "bg-background" : "bg-secondary"}>
+              {row.map((cell, j) => (
+                <TableCell key={j} className="max-w-lg">
+                  {cell}
+                </TableCell>
               ))}
             </TableRow>
-          </TableHeader>
-          <TableBody>
-            {results[0].values.map((row, i) => (
-              <TableRow key={i} className={i % 2 === 0 ? "bg-background" : "bg-secondary"}>
-                {row.map((cell, j) => (
-                  <TableCell key={j} className="max-w-lg">
-                    {cell}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
